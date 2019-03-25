d32407cfee97a7bc722026c957d4990441979688
Robert Pearce <me@robertwpearce.com>
2019-03-26 07:39:54 +1300

    Update all the things! (#29)
    * Various upgrades and additions
    
    * Changelog v2.0.0
    
    * bump eslint to 5.15.3
    
    * VideoPlayer CSS updates
    
    * Adjust progress bar height (smaller) and progress bar dot (bigger)
    
    * bump babels & rm react-dom from peerDependencies
    
    * update CHANGELOG & went ahead and bumped package.json to 2.0.0 (normally would do this once it's in master, but might as well for clarity here, I think)
    
    * Remove unneeded globals bit from .eslintrc.js
    
    * Move prop-types to be a dependency again. @babel/preset-env patch bump
    
    * Update package author field
    
    * use callbacks and this for player events
    
    * rm display: flex from vjs-control-bar so it doesn't always show the bar
    
    * volumeMenuButton to volumePanel
    
    * CSS adjustments
    
    * bump changelog date
    
    * update orson changelog
    
    * rm log

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'array-bracket-spacing': ['error', 'always'],
+    'camelcase': 'error',
+    'comma-dangle': ['error', 'never'],
+    'global-require': 'error',
+    'indent': ['error', 2, { 'SwitchCase': 1 }],
+    'no-console': ['error', { allow: ['error'] }],
+    'no-duplicate-imports': 'error',
+    'no-eq-null': 'error',
+    'no-restricted-globals': ['error', 'event'],
+    'no-template-curly-in-string': 'error',
+    'no-trailing-spaces': 'error',
+    'no-useless-constructor': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'quotes': ['error', 'single', { 'allowTemplateLiterals': true }],
+    'react/display-name': 0,
+    'react/no-deprecated': 0,
+    'react/no-find-dom-node': 0,
+    'react/no-is-mounted': 0,
+    'react/no-string-refs': 0,
+    'react/no-unknown-property': 0,
+    'react/prop-types': 0
+  },
+  settings: {
+    react: {
+      version: '16'
+    }
+  }
+}
