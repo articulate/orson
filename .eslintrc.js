29e7eb2e46f791fec32909926e156877f074d5b2
Robert Pearce <me@robertwpearce.com>
2019-04-17 12:28:26 +1200

    Let's remove all the extra code and see what happens... Also added jsx/a11y but disabled the <track /> caption requirement b/c it would be passed in as a child. Got storybook demo looking good. Aspect ratio needs to be sent to vjs in '16:9' format.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
+    'plugin:jsx-a11y/recommended',
     'plugin:react/recommended'
   ],
   parserOptions: {
@@ -13,19 +14,26 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: [
+    'jsx-a11y',
     'react',
     'react-hooks'
   ],
   rules: {
     'indent': ['error', 2, { 'SwitchCase': 1 }],
     'jsx-quotes': ['error', 'prefer-double'],
+    'jsx-a11y/media-has-caption': [0, {
+      audio: [ 'Audio' ],
+      video: [ 'Video' ],
+      track: [ 'Track' ]
+    }], // caption <track /> would be in child element
     'no-console': ['error', { allow: ['error'] }],
     'no-trailing-spaces': 'error',
     'object-curly-spacing': ['error', 'always'],
     'quotes': ['error', 'single', { 'allowTemplateLiterals': true }],
     'react/prop-types': 0,
     'react-hooks/exhaustive-deps': 'warn',
-    'react-hooks/rules-of-hooks': 'error'
+    'react-hooks/rules-of-hooks': 'error',
+    'semi': ['error', 'never']
   },
   settings: {
     react: {
--- stories/VideoStory.js
+++ stories/VideoStory.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import VideoPlayer from '../lib/VideoPlayer'
 
-const VideoStory = props => {
+const VideoStory = () => {
+  const style = {
+    margin: '40px auto',
+    padding: '20px',
+    position: 'relative',
+    width: '80%'
+  }
+  const opts = {
+    preload: 'none',
+    controlBar: {
+      remainingTimeDisplay: false
+    }
+  }
   return (
-    <VideoPlayer
-      options={{ preload: 'none', controlBar: { remainingTimeDisplay: false } }}
-      poster="https://vjs.zencdn.net/v/oceans.png"
-      {...props}
-    >
-      <source src="https://vjs.zencdn.net/v/oceans.mp4" type="video/mp4" />
-      <source src="https://vjs.zencdn.net/v/oceans.webm" type="video/webm" />
-      <source src="https://vjs.zencdn.net/v/oceans.ogv" type="video/ogg" />
-    </VideoPlayer>
+    <div style={style}>
+      <VideoPlayer options={opts} poster="https://vjs.zencdn.net/v/oceans.png">
+        <source src="https://vjs.zencdn.net/v/oceans.mp4" type="video/mp4" />
+        <source src="https://vjs.zencdn.net/v/oceans.webm" type="video/webm" />
+        <source src="https://vjs.zencdn.net/v/oceans.ogv" type="video/ogg" />
+      </VideoPlayer>
+    </div>
   )
 }
 
