6686b89a0fd2a6a9676acda2098a8e39b0186023
Robert Pearce <me@robertwpearce.com>
2019-04-17 15:09:16 +1200

    updated VideoStory component to make it a little more flexible; added two stories; minor style margin change

--- stories/VideoStory.js
+++ stories/VideoStory.js
@@ -1,28 +1,24 @@
 import React from 'react'
 import VideoPlayer from '../lib/VideoPlayer'
 
-const VideoStory = () => {
-  const style = {
-    margin: '40px auto',
-    padding: '20px',
-    position: 'relative',
-    width: '80%'
-  }
-  const opts = {
-    preload: 'none',
-    controlBar: {
-      remainingTimeDisplay: false
-    }
-  }
-  return (
-    <div style={style}>
-      <VideoPlayer options={opts} poster="https://vjs.zencdn.net/v/oceans.png">
-        <source src="https://vjs.zencdn.net/v/oceans.mp4" type="video/mp4" />
-        <source src="https://vjs.zencdn.net/v/oceans.webm" type="video/webm" />
-        <source src="https://vjs.zencdn.net/v/oceans.ogv" type="video/ogg" />
-      </VideoPlayer>
-    </div>
-  )
+const containerStyle = {
+  margin: '40px auto',
+  padding: '20px',
+  position: 'relative',
+  width: '80%'
 }
+
+const VideoStory = ({
+  options,
+  poster='https://vjs.zencdn.net/v/oceans.png'
+}) => (
+  <div style={containerStyle}>
+    <VideoPlayer options={options} poster={poster}>
+      <source src="https://vjs.zencdn.net/v/oceans.mp4" type="video/mp4" />
+      <source src="https://vjs.zencdn.net/v/oceans.webm" type="video/webm" />
+      <source src="https://vjs.zencdn.net/v/oceans.ogv" type="video/ogg" />
+    </VideoPlayer>
+  </div>
+)
 
 export default VideoStory
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -5,3 +5,14 @@ import VideoStory from './VideoStory'
 
 storiesOf('VideoPlayer', module)
   .add('with defaults', () => <VideoStory />)
+  .add('with some options', () => (
+    <VideoStory
+      options={{
+        preload: 'none',
+        controlBar: {
+          remainingTimeDisplay: false
+        }
+      }}
+    />
+  ))
+  .add('with no poster', () => <VideoStory poster="" />)
